8361fe1769e5f675aa65dcf224b8df40d53f10ec
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-16 10:32:20 -0300

    tarting the authentication layer

---
+++ src/main/java/io/github/dougllasfps/imageliteapi/infra/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package io.github.dougllasfps.imageliteapi.infra.repository;
+
+import io.github.dougllasfps.imageliteapi.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
