0c0dc4d11207b991ff01d769bf38ee252934e879
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-17 16:18:00 -0300

    User API with registration and authentication routes

---
+++ src/main/java/io/github/dougllasfps/imageliteapi/application/users/UserController.java
@@ -0,0 +1,40 @@
+package io.github.dougllasfps.imageliteapi.application.users;
+
+import io.github.dougllasfps.imageliteapi.domain.entity.User;
+import io.github.dougllasfps.imageliteapi.domain.exception.DuplicatedTupleException;
+import io.github.dougllasfps.imageliteapi.domain.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController @RequestMapping("/v1/users") @RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+    @PostMapping
+    public ResponseEntity save(@RequestBody UserDTO dto){
+        try{
+            User user = userMapper.mapToUser(dto);
+            userService.save(user);
+
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }catch (DuplicatedTupleException e) {
+            Map<String,String> jsonResult = Map.of("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(jsonResult);
+        }
+    }
+
+    @PostMapping("/auth")
+    public ResponseEntity authenticate(@RequestBody CredentialsDTO credentials){
+        var token = userService.autheticate(credentials.getEmail(), credentials.getPassword());
+        if(token == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(token);
+    }
+}
