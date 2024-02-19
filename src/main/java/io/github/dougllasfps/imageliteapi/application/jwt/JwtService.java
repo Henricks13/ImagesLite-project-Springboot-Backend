a9735ddec1e815f5108f3af369a41ed0c6602d5a
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-02-19 14:14:42 -0300

    Creating method to obtain email through Token in the backend

--- src/main/java/io/github/dougllasfps/imageliteapi/application/jwt/JwtService.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/application/jwt/JwtService.java
@@ -2,7 +2,7 @@ package io.github.dougllasfps.imageliteapi.application.jwt;
 
 import io.github.dougllasfps.imageliteapi.domain.AccessToken;
 import io.github.dougllasfps.imageliteapi.domain.entity.User;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -47,5 +47,23 @@ public class JwtService {
         claims.put("name", user.getName());
 
         return claims;
+    }
+
+    public String getEmailFromToken (String tokenJwt){
+        try {
+
+
+        JwtParser build = Jwts.parser()
+                .verifyWith(keyGenerator.getKey())
+                .build();
+
+        Jws<Claims> jwsClaims = build.parseSignedClaims(tokenJwt);
+
+        Claims claims = jwsClaims.getPayload();
+
+        return claims.getSubject();
+        }catch (JwtException e){
+            throw new InvalidTokenException(e.getMessage());
+        }
     }
 }
